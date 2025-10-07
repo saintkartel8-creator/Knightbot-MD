c73e7b99c88aafeac979a275c6b31538deec2d3c
saintkartel8-creator <saintkartel8@gmail.com>
2025-10-07 03:19:27 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Prince Romantic🇯🇲🇯🇲🇯🇲', // Your name
+  ownerNumber: '256767511588', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
